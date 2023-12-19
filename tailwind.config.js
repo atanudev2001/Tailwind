d1eb953fc3abea2db2711eee719706974927a366
atanu sarkar <atanu.sarkar@bosenet.com>
2023-12-20 03:22:46 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      loginbackground: '#2C2C2C',
+      white: '#ffffff',
+      savebtn: '#133696'
+    }
+  },
+  plugins: [],
+}
+
